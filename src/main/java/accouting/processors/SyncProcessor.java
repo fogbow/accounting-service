922a30fab4279fc1a822717ebd683e9303bd6158
Gustavo <gustavo.monteiro@ccc.ufcg.edu.br>
2018-09-24 15:14:12 -0300

    Extract method retrive data to improve readability.

--- src/main/java/accouting/processors/SyncProcessor.java
+++ src/main/java/accouting/processors/SyncProcessor.java
@@ -39,15 +39,7 @@ public class SyncProcessor implements Runnable {
 		while(isActive) {			
 			logger.info("Updating tables.");
 			try {
-				this.jdbcTemplate.query(
-						"SELECT * FROM timestamp WHERE order_state=?  OR order_state=?",
-						
-						new Object[] { "FULFILLED", "CLOSED" },
-						(rs, rowNum) -> new Record(rs.getString("orderId"), rs.getString("resourceType"),
-								rs.getString("spec"), rs.getString("userId"), rs.getString("userName"),
-								rs.getString("requestingMember"), rs.getString("providingMember"), rs.getLong("startTime"))
-						
-						).forEach(record -> this.recordRepository.save(record));
+				this.retriveAndSave();
 				
 				logger.info("Finish updating.");
 				
@@ -57,7 +49,17 @@ public class SyncProcessor implements Runnable {
 				isActive = false;
 			}
 		}
-
+	}
+	
+	private void retriveAndSave() {
+		this.jdbcTemplate.query(
+				"SELECT * FROM timestamp WHERE order_state=?  OR order_state=?",
+				
+				new Object[] { "FULFILLED", "CLOSED" },
+				(rs, rowNum) -> new Record(rs.getString("orderId"), rs.getString("resourceType"),
+						rs.getString("spec"), rs.getString("userId"), rs.getString("userName"),
+						rs.getString("requestingMember"), rs.getString("providingMember"), rs.getLong("startTime"))
+				).forEach(record -> this.recordRepository.save(record));
 	}
 
 }
