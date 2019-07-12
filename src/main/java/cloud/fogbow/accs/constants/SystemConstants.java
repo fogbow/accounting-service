ca43f2a2ec6a82f0502d792e9892e1274ef7bf8f
fubica <fubica@dsc.ufcg.edu.br>
2019-07-12 15:33:26 -0300

    Fixed documentation and parameter names

--- src/main/java/cloud/fogbow/accs/constants/SystemConstants.java
+++ src/main/java/cloud/fogbow/accs/constants/SystemConstants.java
@@ -1,10 +1,10 @@
 package cloud.fogbow.accs.constants;
 
 public class SystemConstants {
-    public static final String ACCOUNTING_CONF_FILE = "accounting.conf";
+    public static final String ACCOUNTING_CONF_FILE = "accs.conf";
     public static final String SYSTEM_USER_TOKEN_HEADER_KEY = "Fogbow-User-Token";
     public static final String ID_RECORDER_KEY = "recorder";
     public static final String WHITE_LIST_FILE = "white-list.conf";
-    public static final String SERVICE_BASE_ENDPOINT = "acc/";
+    public static final String SERVICE_BASE_ENDPOINT = "accs/";
 	public static final String API_VERSION_NUMBER = "1.0.0";
 }
